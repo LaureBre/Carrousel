2a994eb3e4ef84f081a8b519f002d3a1efb47f49
LaureBre <acs.laure.eole@gmail.com>
2018-03-16 23:54:12 +0100

    width of images taken into account

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,10 +1,10 @@
-var images = ["lianes", "fougere", "automne", "vallee", "banc", "fleur", "grappes", "mauve", "marches", "piques"];
+var images = ["lianes", "neptune", "tasse", "anneau", "fougere", "automne", "vallee", "banc", "fleur", "grappes", "mauve", "marches", "piques"];
 var domImages = document.getElementById("images");
-var largImg = 307; // images de 267px, marges droite et gauche de 20px
-
+var domConteneur = document.getElementById('conteneur');
+// var largImg = 307; // images de 267px, marges droite et gauche de 20px
+var compteur = 0;
 for (var i=0; i < images.length; i++)
 {
-  console.log("<img src=\'img/_" + images[i] + ".jpg\' alt=\'" + images[i] + "\'>");
   domImages.innerHTML += "<img src=\'img/_" + images[i] + ".jpg\' alt=\'" + images[i] + "\'>";
 }
 
@@ -16,12 +16,16 @@ domRight.addEventListener('click', function() { move(-1); });
 
 function move(dir) {
     var a = domImages.offsetLeft;
-    domLeft.disabled = true;
-    domRight.disabled = true;
-    setTimeout(function(){domRight.disabled = false;}, 1000);
-    setTimeout(function(){domLeft.disabled = false;}, 1000);
-    if (((a > -((images.length-1) * largImg ) ) && (dir == -1)) || ((dir == 1) && (a < -largImg))) {
-        a += largImg * dir;
+    if (((compteur < images.length) && (dir == -1)) || ((dir == 1) && (compteur > 0))) {
+        domLeft.disabled = true;
+        domRight.disabled = true;
+        setTimeout(function(){domRight.disabled = false;}, 1000);
+        setTimeout(function(){domLeft.disabled = false;}, 1000);
+        // var largImg = domImages.childNodes[compteur].width + 40;
+        compteur -= dir;
+        var largImgSuiv = domImages.childNodes[compteur].width + 40;
+        a += largImgSuiv * dir;
         domImages.style.left = a + "px";
+        domConteneur.style.width = largImg + "px";
     }
 }
